c61868d0d251c047808fe8966a1726efefb6ef5e
ahlaiptek <iptekahla@gmail.com>
2024-09-24 10:06:14 +0700

    Melengkapi GIGI_RAPIH

--- src/finals/kesehatan/gigi/GIGI_RAPIH.ts
+++ src/finals/kesehatan/gigi/GIGI_RAPIH.ts
@@ -1,5 +1,8 @@
 enum GIGI_RAPIH {
   GIGI_6_DEPAN_LENGKAP_DAN_TIDAK_CARIES = "6 Gigi Depan Lengkap & Tidak Caries",
+  GIGI_4_SERI_LENGKAP_DAN_TIDAK_CARIES = "4 Gigi Seri Lengkap & Tidak Caries",
+  GIGI_6_SERI_DEPAN_TIDAK_LENGKAP_DAN_CARIES = "6 Gigi Depan Tidak Lengkap & Caries",
+  GIGI_4_SERI_DEPAN_TIDAK_LENGKAP_DAN_CARIES = "4 Gigi Seri Tidak Lengkap & Caries"
 }
 
 export default GIGI_RAPIH;
