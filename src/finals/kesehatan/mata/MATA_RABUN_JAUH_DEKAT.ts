63a762f330613d098f194d8975dbc07b6797d772
ahlaiptek <iptekahla@gmail.com>
2024-09-29 05:56:55 +0700

    Rename

---
+++ src/finals/kesehatan/mata/MATA_RABUN_JAUH_DEKAT.ts
@@ -0,0 +1,6 @@
+enum MATA_RABUN_JAUH_DEKAT {
+  LEBIH_0_5 = "> 0.5",
+  KURANG_0_5 = "< 0.5"
+}
+
+export default MATA_RABUN_JAUH_DEKAT;
--- src/finals/kesehatan/mata/MATA_RABUN_JAUH_OR_DEKAT.ts
+++
@@ -1,6 +0,0 @@
-enum MATA_RABUN_JAUH_OR_DEKAT {
-  MORE_0_5 = "> 0.5",
-  LESS_0_5 = "< 0.5"
-}
-
-export default MATA_RABUN_JAUH_OR_DEKAT;
