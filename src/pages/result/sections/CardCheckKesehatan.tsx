380ce141911870b47646c0ed13b6bc652c19dacb
ahlaiptek <iptekahla@gmail.com>
2024-11-03 07:41:41 +0700

    update (CardCheckKesehatan): Menambah fitur accordion

--- src/pages/result/sections/CardCheckKesehatan.tsx
+++ src/pages/result/sections/CardCheckKesehatan.tsx
@@ -1,4 +1,4 @@
-import { Card, ListGroup } from "react-bootstrap";
+import { Accordion, Card } from "react-bootstrap";
 
 import KesehatanKepala from "./partials/KesehatanKepala";
 import KesehatanMata from "./partials/KesehatanMata";
@@ -16,18 +16,68 @@ function CardCheckKesehatan() {
     <Card>
       <Card.Header>Cek Kesehatan</Card.Header>
       <Card.Body>
-        <ListGroup>
-          <KesehatanMata />
-          <KesehatanKepala />
-          <KesehatanTelinga />
-          <KesehatanHidung />
-          <KesehatanWajah />
-          <KesehatanTangan />
-          <KesehatanTulangPunggung />
-          <KesehatanKaki />
-          <KesehatanLainnya />
-          <KesehatanGigi />
-        </ListGroup>
+        <Accordion>
+          <Accordion.Item eventKey="0">
+            <Accordion.Header>Kesehatan Mata</Accordion.Header>
+            <Accordion.Body>
+              <KesehatanMata />
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="1">
+            <Accordion.Header>Kesehatan Kepala</Accordion.Header>
+            <Accordion.Body>
+              <KesehatanKepala />
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="2">
+            <Accordion.Header>Kesehatan Telinga</Accordion.Header>
+            <Accordion.Body>
+              <KesehatanTelinga />
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="3">
+            <Accordion.Header>Kesehatan Hidung</Accordion.Header>
+            <Accordion.Body>
+              <KesehatanHidung />
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="4">
+            <Accordion.Header>Kesehatan Wajah</Accordion.Header>
+            <Accordion.Body>
+              <KesehatanWajah />
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="5">
+            <Accordion.Header>Kesehatan Tangan</Accordion.Header>
+            <Accordion.Body>
+              <KesehatanTangan />
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="6">
+            <Accordion.Header>Kesehatan Tulang Punggung</Accordion.Header>
+            <Accordion.Body>
+              <KesehatanTulangPunggung />
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="7">
+            <Accordion.Header>Kesehatan Kaki</Accordion.Header>
+            <Accordion.Body>
+              <KesehatanKaki />
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="8">
+            <Accordion.Header>Kesehatan Lainnya</Accordion.Header>
+            <Accordion.Body>
+              <KesehatanLainnya />
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="9">
+            <Accordion.Header>Kesehatan Gigi</Accordion.Header>
+            <Accordion.Body>
+              <KesehatanGigi />
+            </Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
       </Card.Body>
     </Card>
   );
